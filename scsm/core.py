8f90abc5b2e295ce51b30b05c5390b4dad316423
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-12-12 23:55:52 +0000

    Adding tarfile member sanitization to extractall()

--- scsm/core.py
+++ scsm/core.py
@@ -161,7 +161,26 @@ class App():
     def restore(self, backup):
         '''Restore specified backup file'''
         with tarfile.open(Path(self.backup_dir, backup)) as tar:
-            tar.extractall(self.app_dir.parent)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tar, self.app_dir.parent)
 
         if self.config_is_default:
             self.copy_config()
@@ -458,7 +477,26 @@ class SteamCMD():
 
         if pf.system() != 'Windows':
             with tarfile.open(Path(self.directory, f)) as tar:
-                tar.extractall(self.directory)
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
+                    
+                
+                safe_extract(tar, self.directory)
         else:
             with ZipFile(Path(self.directory, f)) as zipf:
                 zipf.extractall(self.directory)
